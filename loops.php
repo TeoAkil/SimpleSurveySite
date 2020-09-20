4d7cff1d756a6c71531c89a0c9d5dbb5334732b3
TeoAkil <ta326@it202-teoakil.c.brave-night-289823.internal>
2020-09-20 20:25:13 +0000

    Added loops homework file

---
+++ loops.php
@@ -0,0 +1,23 @@
+<?php
+echo "All numbers in array";
+echo "<br>\n";
+$arr1 = array(0,1,2,3,4);
+for($i = 0; $i < 5; $i++)
+{
+	echo "$arr1[$i]";
+	echo "<br>\n";
+}
+echo "If you only want the even number";
+echo "<br>\n";
+for($i = 0; $i < 5; $i++)
+{
+	if ($arr1[$1]%2 == 0)
+	{
+		echo "$arr1[$i]";
+		echo "<br>\n";
+	}
+	else
+	{
+	}
+}
+?>
