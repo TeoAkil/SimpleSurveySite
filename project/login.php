6e6c7b4c7e4766256fedbfdacc30ce1fabe37eda
TeoAkil <ta326@it202-teoakil.c.brave-night-289823.internal>
2020-10-21 22:51:30 +0000

    This commit allowed for users to login with an email or username for the site

--- project/login.php
+++ project/login.php
@@ -1,7 +1,9 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
     <form method="POST">
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required/>
+        <input type="email" id="email" name="email" />
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" />
         <label for="p1">Password:</label>
         <input type="password" id="p1" name="password" required/>
         <input type="submit" name="login" value="Login"/>
@@ -10,19 +12,29 @@
 <?php
 if (isset($_POST["login"])) {
     $email = null;
+    $username = null;
     $password = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
+    }
+    if (isset($_POST["username"])) {
+        $username = $_POST["username"];
     }
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
     }
     $isValid = true;
-    if (!isset($email) || !isset($password)) {
+
+    if (!isset($email) && !isset($username)) {
         $isValid = false;
-        flash("Email or password missing");
+        flash("Email/username missing");
     }
-    if (!strpos($email, "@")) {
+
+    if (!isset($password)) {
+        $isValid = false;
+        flash("Password missing");
+    }
+    if (!empty($email) && !strpos($email, "@")) {
         $isValid = false;
         //echo "<br>Invalid email<br>";
         flash("Invalid email");
@@ -30,9 +42,8 @@ if (isset($_POST["login"])) {
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
-
-            $params = array(":email" => $email);
+            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email OR  username = :username LIMIT 1");
+            $params = array(":email" => $email, ":username" => $username);
             $r = $stmt->execute($params);
             //echo "db returned: " . var_export($r, true);
             $e = $stmt->errorInfo();
@@ -44,11 +55,9 @@ if (isset($_POST["login"])) {
             if ($result && isset($result["password"])) {
                 $password_hash_from_db = $result["password"];
                 if (password_verify($password, $password_hash_from_db)) {
-                    $stmt = $db->prepare("
-SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
+                    $stmt = $db->prepare("SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id WHERE UserRoles.user_id = :user_id AND Roles.is_active = 1 AND UserRoles.is_active = 1");
                     $stmt->execute([":user_id" => $result["id"]]);
                     $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
                     unset($result["password"]);//remove password so we don't leak it beyond this page
                     //let's create a session for our user based on the other data we pulled from the table
                     $_SESSION["user"] = $result;//we can save the entire result array since we removed password
