dac784aad004d2bbb49d8d22872cf4dd19f29a0e
TeoAkil <ta326@njit.edu>
2020-12-09 05:07:07 +0000

    This creates a page for the user to view info about the surveys they already took, as well as a nav bar update to get there

---
+++ project/test_view_taken_surveys.php
@@ -0,0 +1,46 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+//if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+   // flash("You don't have permission to access this page");
+    //die(header("Location: login.php"));
+//}
+//
+?>
+<?php
+$sessionid = get_user_id();
+$results = [];
+$db = getDB();
+$stmt = $db->prepare("SELECT DISTINCT s.title,s.description,s.id FROM Survey s JOIN Responses r on s.id = r.survey_id where r.user_id = :sid ");
+$r = $stmt->execute([":sid" => "$sessionid"]);
+if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+else {
+        flash("There was a problem fetching the results");
+    }
+?>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Title:</div>
+                        <div><?php safer_echo($r["title"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Description:</div>
+                        <div><?php safer_echo($r["description"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Survey Id:</div>
+                        <div><?php safer_echo($r["id"]); ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
